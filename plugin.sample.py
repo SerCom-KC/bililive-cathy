29d707679c04537ae67d0b2c833104a553caa5ac
SerCom_KC <szescxz@gmail.com>
2018-01-23 21:36:36 +0800

    Update according to Zap2It API changes

--- plugin.sample.py
+++ plugin.sample.py
@@ -40,7 +40,7 @@ def nowOnAir():
             'time': str(int(time.time())),
             'device': 'X',
             'headendId': 'KY16593',
-            'timespan': '15',
+            'timespan': '3',
             'country': 'USA'
         }
         origlist = requests.get(url, params=params).json()['channels']
@@ -71,39 +71,54 @@ def nextOnAir(text):
     from main import sendDanmaku
     global now_last_query
     global next_last_query
+    time_now = int(time.time())
     if text.replace(' ','') == '#next':
+        maxts = time_now + 3*60*60 # 3 hours from now
         timespan = '3'
     else:
+        maxts = time_now + 12*60*60 # 12 hours from now
         timespan = '12'
         show_name = getShow(text.replace('#next ', ''))
         if show_name == 'ERROR':
             sendDanmaku(u'你输入的命令好像有误的喵~')
             return
-    if timespan == '12' or next_last_query == '' or int(time.time()) > convertTime(now_last_query['endTime']):
+    if isTBS() or timespan == '12' or next_last_query == '' or int(time.time()) > convertTime(now_last_query['endTime']):
         sendDanmaku(u'稍等一下哦，Cathy去查查放送表的喵~')
-        url = "https://tvlistings.zap2it.com/api/grid"
-        params = {
-            'time': str(int(time.time())),
-            'device': 'X',
-            'headendId': 'KY16593',
-            'timespan': timespan,
-            'country': 'USA'
-        }
-        origlist = requests.get(url, params=params).json()['channels']
-        list = False
-        for item in origlist:
-            if item['callSign'] == 'TOON' and (timespan == '12' or not isTBS(convertTime(item['events'][1]['startTime']))):
-                list = item['events']
-                break
-            elif timespan == '3' and item['callSign'] == 'TBS' and isTBS(convertTime(item['events'][1]['startTime'])):
-                list = item['events']
-                break
-        if not list:
-            sendDanmaku(u'Cathy也不知道的喵~')
-            return
-        if not isTBS(convertTime(list[0]['startTime'])):
-            now_last_query = list[0]
-        next_last_query = list[1]
+        for queryts in range(time_now, maxts, 3*60*60):
+            url = "https://tvlistings.zap2it.com/api/grid"
+            params = {
+                'time': str(queryts),
+                'device': 'X',
+                'headendId': 'KY16593',
+                'timespan': '3',
+                'country': 'USA'
+            }
+            origlist = requests.get(url, params=params).json()['channels']
+            list = False
+            for item in origlist:
+                if item['callSign'] == 'TOON' and (timespan == '12' or not isTBS(convertTime(item['events'][1]['startTime']))):
+                    list = item['events']
+                    for item in list:
+                        if item['program']['title'] == show_name and item != list[0]:
+                            sendDanmaku(u'下一次播出时间（北京时间）：')
+                            time.sleep(1)
+                            sendDanmaku(datetime.datetime.fromtimestamp(convertTime(item['startTime'])).strftime('%m月%d日%H:%M'))
+                            if item['program']['episodeTitle'] != None:
+                                time.sleep(1)
+                                sendDanmaku(u'这集的标题是：')
+                                time.sleep(1)
+                                sendDanmaku(item['program']['episodeTitle'])
+                            return
+                elif timespan == '3' and item['callSign'] == 'TBS' and isTBS(convertTime(item['events'][1]['startTime'])):
+                    list = item['events']
+                    break
+            if not list:
+                sendDanmaku(u'Cathy也不知道的喵~')
+                return
+            if not isTBS(convertTime(list[0]['startTime'])) and queryts == time_now:
+                now_last_query = list[0]
+            if queryts == time_now:
+                next_last_query = list[1]
     if timespan == '3':
         sendDanmaku(u'接下来播出的是：')
         time.sleep(1)
@@ -114,17 +129,6 @@ def nextOnAir(text):
             time.sleep(1)
             sendDanmaku(next_last_query['program']['episodeTitle'])
     elif timespan == '12':
-        for item in list:
-            if item['program']['title'] == show_name and item != list[0]:
-                sendDanmaku(u'下一次播出时间（北京时间）：')
-                time.sleep(1)
-                sendDanmaku(datetime.datetime.fromtimestamp(convertTime(item['startTime'])).strftime('%m月%d日%H:%M'))
-                if item['program']['episodeTitle'] != None:
-                    time.sleep(1)
-                    sendDanmaku(u'这集的标题是：')
-                    time.sleep(1)
-                    sendDanmaku(item['program']['episodeTitle'])
-                return
         sendDanmaku(u'12小时内没有发现放送的喵~')
 
 def newOnAir(text):
