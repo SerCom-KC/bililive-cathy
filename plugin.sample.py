f9e75074e469bbe8e5060a534fe6da8b3f452134
SerCom_KC <szescxz@gmail.com>
2018-02-12 23:20:15 +0800

    Bug fixes; fix `#next <id>` command

--- plugin.sample.py
+++ plugin.sample.py
@@ -12,26 +12,57 @@ next_last_query = {'title': '', 'episodeName': '', 'airtime': 0}
 
 def getShow(id):
     id = id.lower()
-    if id == 'tawog':
+    if id == 'tawog' or id == "376453":
         return "The Amazing World of Gumball"
-    elif id == 'cl':
+    elif id == 'cl' or id == "403152":
         return "Clarence"
-    elif id == 'ttg':
+    elif id == 'ttg' or id == "393474":
         return "Teen Titans Go!"
-    elif id == 'tt':
+    elif id == 'tt' or id == "326561":
         return "Teen Titans"
-    elif id == 'uni':
+    elif id == 'uni' or id == "442472":
         return "Unikitty!"
-    elif id == 'at':
+    elif id == 'at' or id == "368594":
         return "Adventure Time"
-    elif id == 'ben10':
+    elif id == 'ben10' or id == "438472":
         return "Ben 10"
-    elif id == 'wbb':
+    elif id == 'wbb' or id == "419032":
         return "We Bare Bears"
-    elif id == 'okko':
+    elif id == 'okko' or id == "440832":
         return "OK K.O.! Let's Be Heroes"
     elif id == 'fs':
         return "Final Space"
+    elif id == 'aao' or id == "444772":
+        return "Apple & Onion"
+    else:
+        return "ERROR"
+
+def getShowID(id):
+    id = id.lower()
+    if id == 'tawog':
+        return "376453"
+    elif id == 'cl':
+        return "403152"
+    elif id == 'ttg':
+        return "393474"
+    elif id == 'tt':
+        return "326561"
+    elif id == 'uni':
+        return "442472"
+    elif id == 'at':
+        return "368594"
+    elif id == 'ben10':
+        return "438472"
+    elif id == 'wbb':
+        return "419032"
+    elif id == 'okko':
+        return "440832"
+    #elif id == 'fs':
+        #return "Final Space"
+    elif id == 'aao':
+        return "444772"
+    elif id.isdigit():
+        return id
     else:
         return "ERROR"
 
@@ -69,16 +100,47 @@ def fixEpisodeName(episode_name):
 
     return episode_name.replace('/', '; ')
 
-def getUSEastTime(format='', yesterday=False):
+def getUSEastTime(format='', yesterday=False, tomorrow=False):
     time_now = datetime.now(pytz.timezone('US/Eastern'))
     time_return = time_now
     if yesterday:
         time_return = time_now - timedelta(days=1)
-    if format == '%d':
-        return time_return.strftime('%d').lstrip('0')
-    if format == '%m/%d/%Y':
-        return time_return.strftime('%m/%d/%Y')
+    elif tomorrow:
+        time_return = time_now + timedelta(days=1)
+    if format == '%d' or format == '%m':
+        return time_return.strftime(format).lstrip('0')
+    if format == '%Y' or format == '%m/%d/%Y':
+        return time_return.strftime(format)
     return time_return
+    
+def getNextShowing(showId):
+    url = 'https://www.adultswim.com/adultswimdynsched/xmlServices/ScheduleServices'
+    params = {
+        'methodName': 'getAllShowingsByID',
+        'showId': showId,
+        'timezone': 'EST'
+    }
+    allShowings = etree.XML(requests.get(url, params=params).content)
+    try:
+        errtmp = allShowings[0]
+    except IndexError:
+        return False # no showings, or invalid showId
+    for show in allShowings:
+        # We need to manually add the year part here.
+        # Since this API usually returns data up to two weeks, we can use this trick:
+        # If it's December, and there's a showing on January, that must be next year's stuff;
+        # Otherwise, this showing should be this year.
+        if getUSEastTime('%m') == '12' and show.xpath('@date')[0].find('January'):
+            year = str(int(getUSEastTime('%Y')) + 1)
+        else:
+            year = getUSEastTime('%Y')
+        date_str = show.xpath('@time')[0] + ' ' + show.xpath('@date')[0] + ' ' + year
+        show_time = pytz.timezone('US/Eastern').localize(datetime.strptime(date_str, '%I:%M %p %B %d %Y'))
+        if int(time.time()) < convertTime(show_time):
+            return {'episodeName': fixEpisodeName(show.xpath('@episode')[0]), 'airtime': convertTime(show_time)}
+            break
+    printlog('ERROR', 'An unexpected error occurred when looking up next showing for showId ' + showId + '.')
+    return False #errors
 
 def checkSchedule(allshows, index, prev_show=''):
     global now_last_query
@@ -87,8 +149,19 @@ def checkSchedule(allshows, index, prev_show=''):
     show_time = pytz.timezone('US/Eastern').localize(datetime.strptime(date_str, '%m/%d/%Y %H:%M'))
     if int(time.time()) < convertTime(show_time):
         # update next_last_query
-        next_last_query['title'] = fixShowName(allshows[index].xpath('@title')[0])
-        next_last_query['episodeName'] = fixEpisodeName(allshows[index].xpath('@episodeName')[0])
+        if allshows[index].xpath('@title')[0] == "Cartoon Network": # fetch episodeName manually
+            next_last_query['title'] = getShow(allshows[index].xpath('@showId')[0])
+            url = 'https://www.adultswim.com/adultswimdynsched/xmlServices/ScheduleServices'
+            params = {
+                'methodName': 'getEpisodeDesc',
+                'showId': allshows[index].xpath('@showId')[0],
+                'episodeId': allshows[index].xpath('@episodeId')[0],
+                'isFeatured': allshows[index].xpath('@isFeatured')[0]
+            }
+            next_last_query['episodeName'] = fixEpisodeName(etree.XML(requests.get(url, params=params).content).xpath("//Desc/episodeDesc/text()")[0])
+        else:
+            next_last_query['title'] = fixShowName(allshows[index].xpath('@title')[0])
+            next_last_query['episodeName'] = fixEpisodeName(allshows[index].xpath('@episodeName')[0])
         next_last_query['airtime'] = convertTime(show_time)
         # update now_last_query
         if prev_show != '':
@@ -97,12 +170,24 @@ def checkSchedule(allshows, index, prev_show=''):
             show = allshows[index-1]
         date_str = show.xpath('@date')[0] + ' ' + show.xpath('@military')[0]
         show_time = pytz.timezone('US/Eastern').localize(datetime.strptime(date_str, '%m/%d/%Y %H:%M'))
-        now_last_query['title'] = fixShowName(show.xpath('@title')[0])
-        now_last_query['episodeName'] = fixEpisodeName(show.xpath('@episodeName')[0])
+        if show.xpath('@title')[0] == "Cartoon Network": # fetch episodeName manually
+            now_last_query['title'] = getShow(show.xpath('@showId')[0])
+            url = 'https://www.adultswim.com/adultswimdynsched/xmlServices/ScheduleServices'
+            params = {
+                'methodName': 'getEpisodeDesc',
+                'showId': show.xpath('@showId')[0],
+                'episodeId': show.xpath('@episodeId')[0],
+                'isFeatured': show.xpath('@isFeatured')[0]
+            }
+            now_last_query['episodeName'] = fixEpisodeName(etree.XML(requests.get(url, params=params).content).xpath("//Desc/episodeDesc/text()")[0])
+        else:
+            now_last_query['title'] = fixShowName(show.xpath('@title')[0])
+            now_last_query['episodeName'] = fixEpisodeName(show.xpath('@episodeName')[0])
         now_last_query['airtime'] = convertTime(show_time)
         return True
+    return False
 
-def getSchedule(channel='undefined', showName='undefined'): # if showName is defined then we need to query 12 hours
+def getSchedule(channel='undefined'):
     from main import sendDanmaku
     sendDanmaku(u'稍等一下哦，Cathy去查查放送表的喵~')
     if channel == 'cn' or channel == 'as' or getChannel() == 'cn' or getChannel() == 'as' or getChannel() == 'offair':
@@ -111,30 +196,44 @@ def getSchedule(channel='undefined', showName='undefined'): # if showName is def
         cn_start = False
         for index, show in enumerate(allshows):
             if show.xpath('@blockName')[0] == 'AdultSwim':
-                continue # skip all [as] entries
+                if not cn_start:
+                    continue # skip all [as] entries
+                else:
+                    prev_show = allshows[index-1] # save the last CN entry of today
+                    break
             if not cn_start: # parse yesterday's [adult swim] schedule, from 0:00
                 cn_start = True
-                url = 'https://www.adultswim.com/adultswimdynsched/asXml/' + getUSEastTime('%d', True) + '.EST.xml'
+                url = 'https://www.adultswim.com/adultswimdynsched/asXml/' + getUSEastTime('%d', yesterday=True) + '.EST.xml'
                 allshows_aswim = etree.XML(requests.get(url).content).xpath('//allshows/show[@date="' + getUSEastTime('%m/%d/%Y') + '"]')
                 for index_aswim, show_aswim in enumerate(allshows_aswim):
-                    if show_aswim == allshows_aswim[-1]: # last [as] entry? save it
-                        prev_show = show_aswim
-                    if checkSchedule(allshows_aswim, index_aswim) and showName == 'undefined':
+                    if checkSchedule(allshows_aswim, index_aswim):
                         return True
+                prev_show = show_aswim          
                 # parse the first entry of CN schedule
                 checkflag = checkSchedule(allshows, index, prev_show)
-                if checkflag and showName == 'undefined':
+                if checkflag:
                     return True
             # parse the rest of CN schedule
             checkflag = checkSchedule(allshows, index)
-            if checkflag and showName == 'undefined':
+            if checkflag:
                 return True
-            if show == allshows[-1]: # parse today's [adult swim] schedule
-                url = 'https://www.adultswim.com/adultswimdynsched/asXml/' + getUSEastTime('%d') + '.EST.xml'
-                allshows_aswim = etree.XML(requests.get(url).content).xpath('//allshows/show[@date="' + getUSEastTime('%m/%d/%Y') + '"]')
-                for index_aswim, show_aswim in enumerate(allshows_aswim):
-                    if checkSchedule(allshows_aswim, index_aswim) and showName == 'undefined':
-                        return True
+        # parse today's [adult swim] schedule
+        url = 'https://www.adultswim.com/adultswimdynsched/asXml/' + getUSEastTime('%d') + '.EST.xml'
+        allshows_aswim = etree.XML(requests.get(url).content).xpath('//allshows/show[@date="' + getUSEastTime('%m/%d/%Y') + '"]')
+        for index_aswim, show_aswim in enumerate(allshows_aswim):
+            if index_aswim == 0:
+                checkflag = checkSchedule(allshows_aswim, index_aswim, prev_show)
+            else:
+                checkflag = checkSchedule(allshows_aswim, index_aswim)
+            if checkflag:
+                return True
+        prev_show = show_aswim
+        # parse the first [as] entry of next day in today's schedule
+        allshows_aswim = etree.XML(requests.get(url).content).xpath('//allshows/show[@date="' + getUSEastTime('%m/%d/%Y', tomorrow=True) + '"]')
+        checkflag = checkSchedule(allshows_aswim, 0, prev_show)
+        if checkflag:
+            return True
+        printlog('ERROR', 'An error occurred when parsing the schedule. The time now is ' + time.ctime())
         return False # not found
 
 def nowOnAir():
@@ -148,8 +247,14 @@ def nowOnAir():
         if not getSchedule():
             sendDanmaku(u'Cathy也不知道的喵~')
             return
+    if now_last_query['title'] == "[AdultSwim]" or now_last_query['title'] == "Cartoon Network":
+        sendDanmaku(u'Cathy也不知道的喵~')
+        return
     sendDanmaku(u'正在播出的是：')
     time.sleep(1)
+    if now_last_query['title'] == "MOVIE" or now_last_query['title'] == "SPECIAL":
+        sendDanmaku(now_last_query['episodeName'])
+        return
     sendDanmaku(now_last_query['title'])
     if now_last_query['episodeName'] != None:
         time.sleep(1)
@@ -159,7 +264,6 @@ def nowOnAir():
 
 def nextOnAir(text):
     from main import sendDanmaku
-    global now_last_query
     global next_last_query
     if text.replace(' ','') == '#next': # literally what's coming up next
         if next_last_query['title'] == '' or int(time.time()) > next_last_query['airtime']:
@@ -169,8 +273,14 @@ def nextOnAir(text):
         if isTBS(next_last_query['airtime']):
             sendDanmaku(u'因为版权原因，短时间内什么都不会播的喵~')
             return
+        if next_last_query['title'] == "[AdultSwim]" or next_last_query['title'] == "Cartoon Network":
+            sendDanmaku(u'Cathy也不知道的喵~')
+            return
         sendDanmaku(u'接下来播出的是：')
         time.sleep(1)
+        if next_last_query['title'] == "MOVIE" or next_last_query['title'] == "SPECIAL":
+            sendDanmaku(next_last_query['episodeName'])
+            return
         sendDanmaku(next_last_query['title'])
         if next_last_query['episodeName'] != None:
             time.sleep(1)
@@ -178,12 +288,23 @@ def nextOnAir(text):
             time.sleep(1)
             sendDanmaku(next_last_query['episodeName'])
     else:
-        show_name = getShow(text.replace('#next ', ''))
-        if show_name == 'ERROR':
+        show_id = getShowID(text.replace('#next ', ''))
+        if show_id == 'ERROR':
             sendDanmaku(u'你输入的命令好像有误的喵~')
             return
-        sendDanmaku(u'呜喵，这个功能被暂时禁用了> <')
-        #getSchedule(showName=show_name)
+        next_showing = getNextShowing(show_id)
+        if next_showing:
+            sendDanmaku(u'下一次播出时间（北京时间）：')
+            time.sleep(1)
+            sendDanmaku(fixTime(next_showing['airtime']))
+            time.sleep(1)
+            sendDanmaku(u'这集的标题是：')
+            time.sleep(1)
+            sendDanmaku(next_showing['episodeName'])
+        else:
+            sendDanmaku(u'在可预见的未来没有发现放送的喵~')
+            if getShow(text.replace('#next ', '')) == 'ERROR':
+                sendDanmaku(u'也许是你输错了数字ID喵？')
 
 def newOnAir(text):
     from main import sendDanmaku
