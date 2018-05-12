b9aa20350537a3099feac80b397b23e21f8f9ef5
SerCom-KC <szescxz@gmail.com>
2018-05-12 09:29:54 +0800

    Check stdout encoding

--- main.py
+++ main.py
@@ -164,9 +164,12 @@ def listenBiliMsg():
 
 def checkConfig(firstrun=False):
     global danmaku_limit
+    if sys.stdout.encoding.lower() != 'utf-8'
+        printlog("ERROR", "Looks like the encoding of stdout is not UTF-8. Try adding PYTHONIOENCODING=utf-8 to your environment variables first.")
+        raise SystemExit
     if getConfig('oauth', 'appkey') == '' or getConfig('oauth', 'appsecret') == '':
         printlog("ERROR", "You must set up OAuth application info in config.ini")
-        quit()
+        raise SystemExit
     checkToken('host', firstrun)
     time.sleep(1)
     checkToken('assist', firstrun)
